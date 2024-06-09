8dc71dfaad57f4cbdd70369e98a32884dadc8e97
Freddie Prianes <fprianes@cspc.edu.ph>
2024-06-10 00:51:29 +0800

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+# This file contains the firebase configuration details.
+firebaseConfig = {
+  "apiKey": "AIzaSyC68therWFedwz4CEbJyM4ywBdgO86BvB8",
+  "authDomain": "irespond-7f0be.firebaseapp.com",
+  "projectId": "irespond-7f0be",
+  "storageBucket": "irespond-7f0be.appspot.com",
+  "messagingSenderId": "502088500304",
+  "appId": "1:502088500304:web:2e1b4a56bee2714dfb7d55",
+  "measurementId": "G-0JYXD4QETG"
+}
